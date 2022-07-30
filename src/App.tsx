251250c34fc071a337de288fed8169c2613f2a51
osasou <osasou0125@gmail.com>
2022-07-30 23:38:39 +0900

    add plateau data

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,18 @@ function App() {
         url="http://127.0.0.1:5174/13101_chiyoda-ku_2020_bldg_low_resolution/tileset.json" // ダウンロードした地図データのパス
         onReady={handleReady}
       />
+      <Cesium3DTileset
+        url="http://127.0.0.1:5174/13102_chuo-ku_2020_bldg_low_resolution/tileset.json" // ダウンロードした地図データのパス
+        onReady={handleReady}
+      />
+      {/* <Cesium3DTileset
+        url="http://127.0.0.1:5174/13102_chuo-ku_2020_bldg_notexture/tileset.json" // ダウンロードした地図データのパス
+        onReady={handleReady}
+      /> */}
+      {/* <Cesium3DTileset
+        url="http://127.0.0.1:5174/13102_chuo-ku_2020_bldg_texture/tileset.json" // ダウンロードした地図データのパス
+        onReady={handleReady}
+      /> */}
     </Viewer>
   );
 }
