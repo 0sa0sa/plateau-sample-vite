34b92c0d86d2023539c5be2d14a9b328dad0a576
osasou <osasou0125@gmail.com>
2022-07-30 22:43:20 +0900

    cesium, resium init setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import cesium from "vite-plugin-cesium";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  // issue: https://github.com/nshen/vite-plugin-cesium/issues/31
+  plugins: [react(), cesium.default()],
+});
