293cd954389bae447b4f64242c22f364c7b036c8
osasou <osasou0125@gmail.com>
2022-07-30 23:06:04 +0900

    init  plateau view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import { Viewer } from "resium";
+// import Cesium, { Cartesian3 } from "cesium";
+// import {
+//   Viewer,
+//   Entity,
+//   PointGraphics,
+//   EntityDescription,
+//   Cesium3DTileset,
+// } from "resium";
+import Cesium from "cesium";
+import { Cesium3DTileset, Viewer } from "resium";
 
 function App() {
-  return <Viewer />;
+  let viewer: Cesium.Viewer; // This will be raw Cesium's Viewer object.
+  const handleReady = (tileset: Cesium.Cesium3DTileset) => {
+    if (viewer) {
+      viewer.zoomTo(tileset);
+    }
+  };
+  return (
+    <Viewer
+      full
+      ref={e => {
+        if (e?.cesiumElement) {
+          viewer = e.cesiumElement;
+        }
+      }}
+    >
+      <Cesium3DTileset
+        url="http://127.0.0.1:5174/13101_chiyoda-ku_2020_bldg_low_resolution/tileset.json" // ダウンロードした地図データのパス
+        onReady={handleReady}
+      />
+    </Viewer>
+  );
 }
 
 export default App;
